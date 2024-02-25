b89bae3fee3173d3d955262215b4e0a9db4f2ea2
okorojijoseph <160272434+okorojijoseph@users.noreply.github.com>
2024-02-24 22:00:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def index():
     return '<h1>Welcome to Wild Rydes</h1><p>Developer: Joseph Okoroji</p><p>Student ID: 100912328</p>'
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
