52b3e7f7589580f8f41cb6474a1b29d00c8951dd
okorojijoseph <160272434+okorojijoseph@users.noreply.github.com>
2024-02-25 03:35:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,1 @@
-from flask import Flask
 
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return '<h1>Welcome to Wild Rydes</h1><p>Developer: Joseph Okoroji</p><p>Student ID: 100912328</p>'
-
-if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 3000)))
